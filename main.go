867e3d73cbee99a1713aeae2ab8262800a5d2866
MRTzee <teukumuksalmina280800@gmail.com>
2024-12-27 14:54:19 +0700

    feat: crud products and db/migration

--- main.go
+++ main.go
@@ -7,6 +7,7 @@ import (
 	"github.com/mrtzee/go-web-native/config"
 	"github.com/mrtzee/go-web-native/controllers/categorycontroller"
 	"github.com/mrtzee/go-web-native/controllers/homecontroller"
+	"github.com/mrtzee/go-web-native/controllers/productcontroller"
 )
 
 func main() {
@@ -15,11 +16,18 @@ func main() {
 	// 1. Homepage
 	http.HandleFunc("/", homecontroller.Welcome)
 
-	// 1. Categories
+	// 2. Categories
 	http.HandleFunc("/categories", categorycontroller.Index)
 	http.HandleFunc("/categories/add", categorycontroller.Add)
 	http.HandleFunc("/categories/edit", categorycontroller.Edit)
 	http.HandleFunc("/categories/delete", categorycontroller.Delete)
+
+	// 3. Products
+	http.HandleFunc("/products", productcontroller.Index)
+	http.HandleFunc("/products/add", productcontroller.Add)
+	http.HandleFunc("/products/detail", productcontroller.Detail)
+	http.HandleFunc("/products/edit", productcontroller.Edit)
+	http.HandleFunc("/products/delete", productcontroller.Delete)
 
 	log.Println("Server running on port 8080")
 	http.ListenAndServe(":8080", nil)
