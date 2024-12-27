e8b2ec0b8e4c2957c0e63a312403ffb0df5e5c64
MRTzee <teukumuksalmina280800@gmail.com>
2024-12-27 15:46:40 +0700

    fix: docker

--- config/database.go
+++ config/database.go
@@ -2,7 +2,9 @@ package config
 
 import (
 	"database/sql"
+	"fmt"
 	"log"
+	"os"
 
 	_ "github.com/go-sql-driver/mysql"
 )
@@ -10,10 +12,27 @@ import (
 var DB *sql.DB
 
 func ConnectDB() {
-	db, err := sql.Open("mysql", "root:password@/mydb?parseTime=true")
+	dbHost := os.Getenv("DB_HOST")
+	dbPort := os.Getenv("DB_PORT")
+	dbUser := os.Getenv("DB_USER")
+	dbPass := os.Getenv("DB_PASSWORD")
+	dbName := os.Getenv("DB_NAME")
+
+	// Format connection string
+	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?parseTime=true",
+		dbUser, dbPass, dbHost, dbPort, dbName)
+
+	db, err := sql.Open("mysql", dsn)
 	if err != nil {
 		panic(err)
 	}
+
+	// Test koneksi
+	err = db.Ping()
+	if err != nil {
+		panic(err)
+	}
+
 	log.Println("Database connected")
 	DB = db
 }
